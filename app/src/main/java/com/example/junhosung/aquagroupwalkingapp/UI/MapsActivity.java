002096f43fca45db104bd4c2d1ffdc712173dae5
oyip29 <33005770+oyip29@users.noreply.github.com>
2018-03-10 14:35:28 -0800

    Working on real time location to replace the hardcoded location. A world map loads for now if working properly

--- app/src/main/java/com/example/junhosung/aquagroupwalkingapp/UI/MapsActivity.java
+++ app/src/main/java/com/example/junhosung/aquagroupwalkingapp/UI/MapsActivity.java
@@ -1,10 +1,17 @@
 package com.example.junhosung.aquagroupwalkingapp.UI;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import com.example.junhosung.aquagroupwalkingapp.model.SharedPreferenceLoginState;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -13,25 +20,25 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-
 import com.example.junhosung.aquagroupwalkingapp.R;
 import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
     private GoogleMap mapDisplay;
-    // private SupportMapFragment mapFrag;
-
+    Circle myRadius;
+    private LocationManager locationManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        MapFragment mapFrag = ((MapFragment) getFragmentManager().findFragmentById(R.id.mapFrag));
-        mapFrag.getMapAsync(this);
+        setUpLogoutBtn();
         Button btn = (Button) findViewById(R.id.monitorbtn);
         btn.setOnClickListener(new OnClickListener() {
             @Override
@@ -40,21 +47,128 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 startActivity(intent);
             }
         });
-        // Button updateButton = (Button)findViewById(R.id.btnUpdate);
-        setUpLogoutBtn();
+        MapFragment mapFrag = ((MapFragment) getFragmentManager().findFragmentById(R.id.mapFrag));
+        mapFrag.getMapAsync(this);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        // choose between using network provider or gps provider since android chooses between the two
+        if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 10, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                    Geocoder geocoder = new Geocoder(getApplicationContext());
+                    try {
+                        geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 0);
+                        mapDisplay.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                        mapDisplay.addMarker(new MarkerOptions().position(currentLocation).title("I'm here"));
+                        mapDisplay.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
+                        CameraUpdate defaultDisplay = CameraUpdateFactory.newLatLngZoom(currentLocation, 17);
+                        mapDisplay.animateCamera(defaultDisplay);
+                        myRadius = mapDisplay.addCircle(new CircleOptions()
+                                .center(currentLocation)
+                                .radius(100)
+                                .strokeColor(Color.BLUE)
+                                .fillColor(Color.TRANSPARENT));
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+                @Override
+                public void onStatusChanged(String s, int i, Bundle bundle) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String s) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String s) {
+
+                }
+            });
+        }
+        else{
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 2000, 10, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                    Geocoder geocoder = new Geocoder(getApplicationContext());
+                    try {
+                        geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 0);
+                        mapDisplay.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                        mapDisplay.addMarker(new MarkerOptions().position(currentLocation).title("I'm here"));
+                        mapDisplay.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
+                        CameraUpdate defaultDisplay = CameraUpdateFactory.newLatLngZoom(currentLocation, 17);
+                        mapDisplay.animateCamera(defaultDisplay);
+                        myRadius = mapDisplay.addCircle(new CircleOptions()
+                                .center(currentLocation)
+                                .radius(100)
+                                .strokeColor(Color.BLUE)
+                                .fillColor(Color.TRANSPARENT));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onStatusChanged(String s, int i, Bundle bundle) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String s) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String s) {
+
+                }
+            });
+        }
 
     }
-    @Override
+
+    //public void onMapReady(GoogleMap googleMap){
+
+   // }
+
+   @Override
     public void onMapReady(GoogleMap googleMap) {
         mapDisplay = googleMap;
-        LatLng defaultLocation = new LatLng(49.2827, -123.1207);
+       /*LatLng defaultLocation = new LatLng(49.2827, -123.1207);
         mapDisplay.setMapType(GoogleMap.MAP_TYPE_HYBRID);
         mapDisplay.addMarker(new MarkerOptions().position(defaultLocation).title("I'm here"));
         mapDisplay.moveCamera(CameraUpdateFactory.newLatLng(defaultLocation));
-        CameraUpdate defaultDisplay = CameraUpdateFactory.newLatLngZoom(defaultLocation, 16);
+        CameraUpdate defaultDisplay = CameraUpdateFactory.newLatLngZoom(defaultLocation, 17);
         mapDisplay.animateCamera(defaultDisplay);
-
+        myRadius = mapDisplay.addCircle(new CircleOptions()
+                .center(defaultLocation)
+                .radius(100)
+                .strokeColor(Color.BLUE)
+                .fillColor(Color.TRANSPARENT));*/
     }
+    /*@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_maps, menu);
+        return true;
+    }*/
 
     private void setUpLogoutBtn() {
         Button btn = (Button) findViewById(R.id.btnLogout);
