40542ed533482067cab8b9d8612ea94df404dcb7
oyip29 <33005770+oyip29@users.noreply.github.com>
2018-03-23 11:33:55 -0700

    30 seconds timer, call to get array, set my location from coordinates to server. Need to add extra button to access Parent view

--- app/src/main/java/com/example/junhosung/aquagroupwalkingapp/UI/MapsActivity.java
+++ app/src/main/java/com/example/junhosung/aquagroupwalkingapp/UI/MapsActivity.java
@@ -8,10 +8,13 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.CountDownTimer;
+import android.os.SystemClock;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.junhosung.aquagroupwalkingapp.model.GpsLocation;
 import com.example.junhosung.aquagroupwalkingapp.model.Group;
 import com.example.junhosung.aquagroupwalkingapp.model.Model;
 import com.example.junhosung.aquagroupwalkingapp.model.SharedPreferenceLoginState;
@@ -26,7 +29,9 @@ import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.junhosung.aquagroupwalkingapp.R;
@@ -44,11 +49,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
+import java.util.Timer;
 
 import static com.example.junhosung.aquagroupwalkingapp.model.Model.getInstance;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
     Model model = Model.getInstance();
+    User currentUser;
     private GoogleMap mapDisplay;
     Group currentGroup;
     Circle myRadius;
@@ -60,12 +67,19 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     List<List<Double>> LngList;
     List<Double> setLatList;
     List<Double> setLngList;
-
+    long elapsedTime;
+    Chronometer Timer = null;
+    int minElapsed = 0;
+    int secondElapsed = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        TextView updateDisplay = (TextView) findViewById(R.id.textViewUpdate);
+        TextView updateTime = (TextView) findViewById(R.id.textViewTimeUpdate);
+        ((TextView) findViewById(R.id.curGroupInMaps)).setText(currentGroup.getGroupDescription());//("Yipper group");
+        updateTime.setText(Integer.toString(minElapsed) + " Minutes: " + Integer.toString(secondElapsed) + " Seconds");
         setUpUpdateBtn();
         setUpLogoutBtn();
         setUpViewGroupBtn();
@@ -81,6 +95,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         MapFragment mapFrag = ((MapFragment) getFragmentManager().findFragmentById(R.id.mapFrag));
         mapFrag.getMapAsync(this);
         locationUpdate();
+     //   locationTimer();
     }
 
     // circle now set 500 meter radius from myself
@@ -99,6 +114,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                     if (myRadius != null || marker != null) {
                         mapDisplay.clear();
                     }
+
                     try {
                         geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 0);
                         Toast.makeText(getApplicationContext(), "Our location is Latitude: " + location.getLatitude() + "  Longitude: " + location.getLongitude(), Toast.LENGTH_LONG).show();
@@ -106,6 +122,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                         marker = new MarkerOptions().position(currentLocation).title("We're here");
                         mapDisplay.addMarker(marker);
                         markGroupsOnMap();
+                        // displayTimeSinceLastUpdate();
                         mapDisplay.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
                         CameraUpdate defaultDisplay = CameraUpdateFactory.newLatLngZoom(currentLocation, 17);
                         mapDisplay.animateCamera(defaultDisplay);
@@ -222,6 +239,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         updateButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
+                displayTimeSinceLastUpdate();
                 locationUpdate();
             }
         });
@@ -229,29 +247,64 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     private void markGroupsOnMap() {
         int i;
-        Double[] latitudeList = {49.2826, 49.2825, 49.2818,49.2819};
-        Double[] longitudeList = {-123.1206, -123.1209, -123.1219,-123.1221};
+        Double[] latitudeList = {49.2826, 49.2825, 49.2818, 49.2819};
+        Double[] longitudeList = {-123.1206, -123.1209, -123.1219, -123.1221};
         setLatList = Arrays.asList(latitudeList);
         setLngList = Arrays.asList(longitudeList);
         String[] groupList = {"Yipper Group", "group2", "Big Daddy's group","yipper"};
         for (i = 0; i < latitudeList.length; i++) {
-                    LatLng markLocation = new LatLng(latitudeList[i], longitudeList[i]);
-                    mapDisplay.addMarker(groupMarker = new MarkerOptions().position(markLocation).title(groupList[i]).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET)));
+            LatLng markLocation = new LatLng(latitudeList[i], longitudeList[i]);
+            mapDisplay.addMarker(groupMarker = new MarkerOptions().position(markLocation).title(groupList[i]).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET)));
 
         }
     }
-    private void callbackGroupDetails(List<User> group){
-        List<String> groupUsersDisplayList = new ArrayList<>();
-        for(User thisUser: group){
-            groupUsersDisplayList.add(thisUser.getName()); //+" , "+ thisUser.getEmail());
-            Log.v("","something is here----------------------------------");
-        }
 
+    private void displayTimeSinceLastUpdate() {
+        long minutes = ((SystemClock.elapsedRealtime() - Timer.getBase()) / 1000) / 60;
+        long seconds = ((SystemClock.elapsedRealtime() - Timer.getBase()) / 1000) % 60;
+        elapsedTime = SystemClock.elapsedRealtime();
+        Log.d("Check Time", "Since last location update: " + minutes + " : " + seconds);
     }
-    private void getCurrentGroup(Group group){
+
+   private void sendGroupCurrentLocation(Group group) {
         currentGroup = group;
-        model.getMembersOfGroup(model.getCurrentGroupInUseByUser().getId(),this::callbackGroupDetails);
+        currentUser = currentGroup.getLeader();
+        Double[] curlat = {49.2826, 49.2827};
+        Double[] curlng = {-123.1206, 49.2728};
+        GpsLocation myCurrentLocation = new GpsLocation();
+        myCurrentLocation.setLat(49.2828);
+        myCurrentLocation.setLng(-123.1208);
+        currentUser.setLastGpsLocation(myCurrentLocation);
+        setLatList = Arrays.asList(curlat);
+        setLngList = Arrays.asList(curlng);
+        currentGroup.setRouteLatArray(setLatList);
+        currentGroup.setRouteLngArray(setLngList);
     }
-    private void callbackSetGpsCoordinates(Group group){
+
+    private void sendLocationSever() {
+        model.updateGroupDetails(currentGroup.getId(), currentGroup, this :: sendGroupCurrentLocation);
+    }
+
+    private void getCoordinates() {
+        model.getGroupDetailsById(currentGroup.getId(), this :: groupAttributesCallback);
+    }
+
+    private void groupAttributesCallback(Group group) {
+        model.getCurrentGroupInUseByUser().getGroupDescription();
+        currentGroup = group;
+        currentUser = currentGroup.getLeader();
+        currentUser.getLastGpsLocation();
+        currentGroup.getRouteLatArray();
+        currentGroup.getRouteLngArray();
+    }
+
+    private void locationTimer() {
+        CountDownTimer timer = new CountDownTimer(30000, 1) {
+            public void onTick(long millisUntilFinished) {
+            }
+            public void onFinish() {
+                locationUpdate();
+            }
+        }.start();
     }
 }
