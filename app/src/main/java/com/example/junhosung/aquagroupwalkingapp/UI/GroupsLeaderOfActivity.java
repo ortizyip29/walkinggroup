2a5814070d6d54a3ef3d359ed46ca9a076509830
Kartik Jain <kartikj@sfu.ca>
2018-03-25 22:56:27 -0700

    fixed create group and modify group and change group

--- app/src/main/java/com/example/junhosung/aquagroupwalkingapp/UI/GroupsLeaderOfActivity.java
+++ app/src/main/java/com/example/junhosung/aquagroupwalkingapp/UI/GroupsLeaderOfActivity.java
@@ -19,6 +19,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+
 public class GroupsLeaderOfActivity extends AppCompatActivity {
 
     Model model = Model.getInstance();
